9e440c040b7f17e1deabfc2a91456efa2e529d80
lior Shine <shinelior@gmail.com>
2020-06-02 07:27:47 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+
+class Person 
+  
+def initialize(name)
+   @name = name
+ end
+def name
+  @name
+ end
+ #def name = (new_name)
+  # @name = new_name
+#end
+end
+#adele_goldberg.name = "adele_goldberg"
+
+adele_goldberg = Person.new("adele_goldberg")
+adele_goldberg.name
+
+alan_kay = Person.new("alan_kay")
+
+
+alan_kay.name
